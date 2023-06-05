d97535517aaef4c1021a98ac02de26662c5b3ae8
Yura <Orl0ff.1ur@yandex.ru>
2023-06-06 00:01:19 +0300

    add functions

---
+++ units/functions.py
@@ -0,0 +1,33 @@
+import json
+def open_json():
+    """
+    открываем файл json
+    :return: [{}]
+    """
+    pass
+
+def sort_date(date):
+    """
+    сортировка данных по дате
+    :return:отсортированный список по дате
+    """
+    pass
+
+def mask_number_card(number_card):
+    """
+    маскировка номеров карт
+    :return:отсортированный номер карты
+    """
+    pass
+
+def mask_number_score(number_score):
+    """
+    маскировка номера счета
+    :param number_score:
+    :return: отсортированный номер счета
+    """
+    pass
+
+
+
+
