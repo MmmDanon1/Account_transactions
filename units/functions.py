d817033a508f28c5a4fbfb655db36b9a48dbc4c5
Yura <Orl0ff.1ur@yandex.ru>
2023-06-07 00:29:16 +0300

    fixed functions.py code is writen main.py

--- units/functions.py
+++ units/functions.py
@@ -14,7 +14,15 @@ def sort_data(data):
     сортировка данных
     :return:отсортированный список данных
     """
-    sort_data_list = [operation for operation in data if operation["state"] == "EXECUTED"]
+
+    # sort_data_list = [operation for operation in data if operation["state"] == "EXECUTED"]
+    sort_data_list = []
+    for operation in data:
+        try:
+            if operation["state"] == "EXECUTED":
+                sort_data_list.append(operation)
+        except:
+            continue
 
     return sort_data_list
 
@@ -23,17 +31,22 @@ def mask_number_card(number_card):
     маскировка номеров карт
     :return:отсортированный номер карты
     """
-    for number in number_card:
-        sort_numbers_card = number["from"]
-        mask = list(sort_numbers_card)
-        mask[-5] = "*"
-        mask[-6] = "*"
-        mask[-7] = "*"
-        mask[-8] = "*"
-        mask[-9] = "*"
-        mask[-10] = "*"
+    # for number in number_card:
+    try:
+       sort_numbers_card = number_card["from"]
+       mask = list(sort_numbers_card)
+       mask[-5] = "*"
+       mask[-6] = "*"
+       mask[-7] = "*"
+       mask[-8] = "*"
+       mask[-9] = "*"
+       mask[-10] = "*"
 
-        return("".join(mask))
+       return ("".join(mask))
+
+    except:
+
+       return "Номер карты неизвестен"
 
 def mask_number_score(number_score):
     """
@@ -41,16 +54,20 @@ def mask_number_score(number_score):
     :param number_score:
     :return: отсортированный номер счета
     """
-    for number in number_score:
-        sort_number_score = number["to"]
-
+    # for number in number_score:
+    try:
+        sort_number_score = number_score["to"]
         sort_number_score_element = sort_number_score.split(" ")
-        mask_number_score = list(sort_number_score_element[1])
+        mask_number_score = list(sort_number_score_element[-1])
         mask_number_score[-5] = "*"
         mask_number_score[-6] = " *"
         number_slice = mask_number_score[-6:]
 
         return sort_number_score_element[0] + "".join(number_slice)
+
+    except:
+
+        return "Номер карты неизвестен"
 
 def sort_data_by_date(data):
     """
